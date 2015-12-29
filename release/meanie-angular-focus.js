c85f700fde4367e3827ab01495465935d2f8b13d
Adam Buczynski <adambuczynski@gmail.com>
2015-12-29 16:06:59 +1300

    Bump version to 1.0.3

--- release/meanie-angular-focus.js
+++ release/meanie-angular-focus.js
@@ -1,5 +1,5 @@
 /**
- * meanie-angular-focus - v1.0.2 - 29-11-2015
+ * meanie-angular-focus - v1.0.3 - 29-11-2015
  * https://github.com/meanie/angular-focus
  *
  * Copyright (c) 2015 Adam Buczynski <me@adambuczynski.com>
@@ -17,14 +17,13 @@ angular.module('Focus.Service', [])
  */
 .factory('$focus', ['$timeout', '$window', function($timeout, $window) {
   return function(element, timeout) {
-    if (typeof element === 'string') {
-      element = $window.document.getElementById(element);
-    }
-    if (!element || typeof element.focus !== 'function') {
-      return;
-    }
     $timeout(function() {
-      element.focus();
+      if (typeof element === 'string') {
+        element = $window.document.getElementById(element);
+      }
+      if (element && typeof element.focus === 'function') {
+        element.focus();
+      }
     }, timeout || 0);
   };
 }]);
