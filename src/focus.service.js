8b9537284e9cfb85a7d82ff6bfe871b82870ecd0
Adam Buczynski <adambuczynski@gmail.com>
2016-01-02 09:50:09 +1300

    Support for jQuery elements

--- src/focus.service.js
+++ src/focus.service.js
@@ -10,9 +10,18 @@ angular.module('Focus.Service', [])
 .factory('$focus', function($timeout, $window) {
   return function(element, timeout) {
     $timeout(function() {
+
+      //String ID
       if (typeof element === 'string') {
         element = $window.document.getElementById(element);
       }
+
+      //jQuery element
+      if (typeof element[0] !== 'undefined') {
+        element = element[0];
+      }
+
+      //HTML DOM element
       if (element && typeof element.focus === 'function') {
         element.focus();
       }
